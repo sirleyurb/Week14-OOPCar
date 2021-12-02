057716cf67521f5fe5a6c357ee0adab94e2c515d
Opilane TTHK <opilane@tthk.ee>
2021-12-02 10:01:09 +0200

    Week14

---
+++ OOPCar/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace OOPCar
+{
+    class Program
+    {
+        class Car
+        {
+            string mark;
+            string model;
+            string regNumber;
+            string color;
+            int odo;
+            int fuelTank;
+
+            public Car(string _mark, string _model, string _regNumber, string _color)
+            {
+                mark = _mark;
+                model = _model;
+                regNumber = _regNumber;
+                color = _color;
+                odo = 0;
+                fuelTank = 60;
+
+                Console.WriteLine($"{color} {mark} {model} {regNumber} has been created.");
+            }
+
+
+            public int FuelTank
+            {
+                get { return fuelTank; }
+            }
+
+
+            public void Drive()
+            {
+                fuelTank -= 5;
+                odo += 100;
+                Console.WriteLine("Vroom-vroom");
+            }
+
+            public void ShowCarData()
+            {
+                Console.WriteLine($"Mark: {mark}");
+                Console.WriteLine($"Model: {model}");
+                Console.WriteLine($"Registration Number: {regNumber}");
+                Console.WriteLine($"Color: {color}");
+                Console.WriteLine($"Odometer: {odo}");
+                Console.WriteLine($"Fuel: {fuelTank}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Toyota", "Celica", "777BTJ", "Red");
+
+            while (myCar.FuelTank > 0)
+            {
+                myCar.Drive();
+            }
+
+            myCar.ShowCarData();
+        }
+    }
+}
